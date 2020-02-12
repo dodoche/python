8fd161d269e582dcb514aab83ed7e9f199937bbf
dodoche <doralyfr@yahoo.fr>
2020-02-12 11:01:35 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,32 @@
 '''
-Created on 04-Sep-2019
 
-@author: bkadambi
-'''
+import openshift as oc
 
-# -*- coding: UTF-8 -*-
-"""
-hello_flask: First Python-Flask webapp
-"""
-from flask import Flask  # From module flask import class Flask
-app = Flask(__name__)    # Construct an instance of Flask class for our webapp
+print('OpenShift client version: {}'.format(oc.get_client_version()))
+print('OpenShift server version: {}'.format(oc.get_server_version()))
 
-@app.route('/')   # URL '/' to be handled by main() route handler
-def main():
-    """Say hello"""
-    return 'Hello, world!'
-
-if __name__ == '__main__':  # Script executed directly?
-    print("Hello World! Built with a Docker file.")
-    app.run(host="0.0.0.0", port=8080, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
+# Set a project context for all inner `oc` invocations and limit execution to 10 minutes
+with oc.project('openshift-infra'), oc.timeout(10*60):
+    # Print the list of qualified pod names (e.g. ['pod/xyz', 'pod/abc', ...]  in the current project
+    print('Found the following pods in {}: {}'.format(oc.get_project_name(), oc.selector('pods').qnames()))
+    
+    # Read in the current state of the pod resources and represent them as python objects
+    for pod_obj in oc.selector('pods').objects():
+        
+        # The APIObject class exposes several convenience methods for interacting with objects
+        print('Analyzing pod: {}'.format(pod_obj.name()))
+        pod_obj.print_logs(timestamps=True, tail=15)
+    
+        # If you need access to the underlying resource definition, get a Model instance for the resource
+        pod_model = pod_obj.model
+        
+        # Model objects enable dot notation and allow you to navigate through resources
+        # to an arbitrary depth without checking if any ancestor elements exist.
+        # In the following example, there is no need for boilerplate like:
+        #    `if .... 'ownerReferences' in pod_model['metadata'] ....`
+        # Fields that do not resolve will always return oc.Missing which 
+        # is a singleton and can also be treated as an empty dict.
+        for owner in pod_model.metadata.ownerReferences:  # ownerReferences == oc.Missing if not present in resource
+            # elements of a Model are also instances of Model or ListModel
+            if owner.kind is not oc.Missing:  # Compare as singleton
+                print '  pod owned by a {}'.format(owner.kind)  # e.g. pod was created by a StatefulSet
