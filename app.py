c154b9dec193a43c815057d10ddd248b248ca33e
dodoche <doralyfr@yahoo.fr>
2020-02-12 23:33:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,14 @@ def main():
   api_instance = client.CoreV1Api()
   cmap = client.V1ConfigMap()
   cmap.metadata = client.V1ObjectMeta(name="special-config1")
-  print("test1")
   cmap.data = {}
   cmap.data["special.how"] = "very"
   cmap.data["special.type"] = "charm"
   cmap.data["special.creationTimestamp"]= "2016-02-18T18:52:05Z"
   cmap.data["special.namespace"] = "default"
-  print("test2")
-  #api_instance.create_namespaced_config_map(namespace="default", body=cmap)
+  api_instance.create_namespaced_config_map(namespace="tete", body=cmap)
   # print(cmap.data)
-  print(cmap)
+  )
 
 if __name__ == '__main__':
     main()
