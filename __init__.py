b0c2ed5ed9c2ec874a022484dbea3b33b3c5b15a
dodoche <doralyfr@yahoo.fr>
2020-02-23 14:58:40 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from .context import *
+from .base_verbs import *
+from .model import OpenShiftPythonException
+from .model import Model, Missing
+from .selector import *
+from .apiobject import *
+from . import naming
+from . import status
+from . import config
+from .ansible import ansible
+null = None  # Allow scripts to specify null in object definitions
+
+
+# Allows modules to trigger errors
+def error(msg, **kwargs):
+    raise OpenShiftPythonException(msg, **kwargs)
