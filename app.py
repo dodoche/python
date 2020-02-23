e846bd756d22d92edd1153addc76dedbc2db8afa
dodoche <doralyfr@yahoo.fr>
2020-02-23 15:02:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
 from __future__ import print_function
+from __future__ import absolute_import
+import openshift as oc
+from contextlib import contextmanager
+import argparse
 import time
-import kubernetes.client
-from kubernetes.client.rest import ApiException
-from pprint import pprint
+import logging
+import traceback
 
-# Configure API key authorization: BearerToken
-configuration = kubernetes.client.Configuration()
-#configuration.api_key['authorization'] = 'YOUR_API_KEY'
-# Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
-# configuration.api_key_prefix['authorization'] = 'Bearer'
+def main():
 
-# create an instance of the API class
-api_instance = kubernetes.client.CoreV1Api(kubernetes.client.ApiClient(configuration))
-name = "elastic-licensing" # str | name of the ConfigMap
-namespace = 'default' # str | object name and auth scope, such as for teams and projects
-pretty = 'pretty_example' # str | If 'true', then the output is pretty printed. (optional)
-exact = True # bool | Should the export be exact.  Exact export maintains cluster-specific fields like 'Namespace'. Deprecated. Planned for removal in 1.18. (optional)
-export = True # bool | Should this value be exported.  Export strips fields that a user can not specify. Deprecated. Planned for removal in 1.18. (optional)
+  configmaps =  oc.selector("configmaps")
+  Configmaps = configmaps.objects()
+  conf = Configmaps[2]
+  print('Annotations:\n{}\n'.format(conf.model.metadata))
 
-try:
-    api_response = api_instance.read_namespaced_config_map(name, namespace, pretty=pretty, exact=exact, export=export)
-    pprint(api_response)
-except ApiException as e:
-    print("Exception when calling CoreV1Api->read_namespaced_config_map: %s\n" % e)
+if __name__ == '__main__':
+  main()
 
 
