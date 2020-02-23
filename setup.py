9f538494ca5c3ba438231c17b7359a8295cc165a
dodoche <doralyfr@yahoo.fr>
2020-02-23 14:49:14 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sukem',
+    version='0.0.1',
+    author='doraly',
+    url="https://github.com/dodoche/openshift-python.git",
+    packages=find_packages('selector.py'),
+    description='Python sample application'
